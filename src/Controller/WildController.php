0cc5896e4236540c365ab9343f87a2944675d017
dartagnan45 <dartagnan45@gmail.com>
2020-05-05 18:41:32 +0200

    quete routing avancé

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -17,4 +17,29 @@ class WildController extends AbstractController
             'website' => 'Wild Séries',
         ]);
     }
+
+    /**
+     * @Route("wild/show/{slug<[a-zA-Z0-9- -_-]+>}", name="wild_show")
+     */
+    public function show(?string $slug = null, ?string $error = null): Response
+    {
+        if (preg_match('#\_#', $slug)) {
+            return $this->redirectToRoute('wild_error');
+        } elseif (!$slug) {
+            $error = "Aucune série sélectionnée, veuillez choisir une série";
+        }
+        return $this->render('wild/show.html.twig', [
+            'slug' => ucwords(str_replace(['-'], ' ', $slug)),
+            'error' => $error
+        ]);
+    }
+
+
+    /**
+     * @Route("wild/error", name="wild_error")
+     */
+    public function error(): Response
+    {
+        return $this->render('wild/404.html.twig');
+    }
 }
